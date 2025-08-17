620f234d09fa4d800e546a2bb18a4d5b13dd6538
unnati779 <unnatibhattarai2002@gmail.com>
2025-08-17 14:45:35 +0545

    Update 01-descriptive statistics.R

--- 01-descriptive statistics.R
+++ 01-descriptive statistics.R
@@ -1,1 +1,20 @@
+install.packages("readxl")
+install.packages("psych")
+# Load libraries
+library(readxl)
+library(psych)
+
+# Import data
+data <- read_excel("C:/Users/DEll/Downloads/without PCR.xlsx", sheet = "Sheet1")
+
+# Select only DV and IVs
+data <- data[, c("NPL_log",
+                 "board independence", "board.meetings", "concentrated.ownership",
+                 "gender.diversity", "lending.rate", "unemployment.rate",
+                 "remittance", "credit.to.deposit", "bank.size", "bank.car",
+                 "first.merger", "second.merger")]
+
+# Descriptive Statistics
+desc_stats <- psych::describe(data)
+print(desc_stats)
 
