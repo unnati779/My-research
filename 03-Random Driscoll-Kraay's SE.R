aa8cc765c5d9782add69115efb46d1abe34d1dde
unnati779 <unnatibhattarai2002@gmail.com>
2025-08-19 12:19:39 +0545

    Update 03-Random Driscoll-Kraay's SE.R

--- 03-Random Driscoll-Kraay's SE.R
+++ 03-Random Driscoll-Kraay's SE.R
@@ -1,1 +1,20 @@
+# install.packages(c("plm", "lmtest", "sandwich"))
 
+library(plm)
+library(lmtest)
+library(sandwich)
+
+# Random Effects model
+re_model <- plm(
+  NPL_log ~ board.independence + board.meetings + concentrated.ownership + gender.diversity +
+    lending.rate + unemployment.rate + remittance + credit.to.deposit +
+    bank.size + bank.car + first.merger + second.merger,
+  data = pdata,
+  model = "random"
+)
+
+# Driscoll-Kraay Robust Standard Errors
+dk_se <- vcovSCC(re_model, type = "HC1", cluster = "time")
+
+# Show results with DK SEs
+coeftest(re_model, vcov = dk_se)
