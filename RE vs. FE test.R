0ae493279bd5f8883ca44d122ae5edb8a8512eab
unnati779 <unnatibhattarai2002@gmail.com>
2025-08-19 12:20:40 +0545

    Create RE vs. FE test.R

---
+++ RE vs. FE test.R
@@ -0,0 +1,32 @@
+# install.packages(c("plm", "lmtest", "readxl"))
+
+library(plm)
+library(lmtest)
+library(readxl)
+
+# Load your data
+df <- read_excel("C:/Users/DEll/Downloads/Research final data.xlsx", sheet = "Sheet1")
+
+# Declare panel structure
+pdata <- pdata.frame(df, index = c("bank.id", "fiscal.year"))
+# Fixed Effects model
+fe_model <- plm(
+  NPL_log ~ board.independence + board.meetings + concentrated.ownership + gender.diversity +
+    lending.rate + unemployment.rate + remittance + credit.to.deposit +
+    bank.size + bank.car + first.merger + second.merger,
+  data = pdata,
+  model = "within"
+)
+
+# Random Effects model
+re_model <- plm(
+  NPL_log ~ board.independence + board.meetings + concentrated.ownership + gender.diversity +
+    lending.rate + unemployment.rate + remittance + credit.to.deposit +
+    bank.size + bank.car + first.merger + second.merger,
+  data = pdata,
+  model = "random"
+)
+hausman_test <- phtest(fe_model, re_model)
+print(hausman_test)
+lm_test <- plmtest(re_model, type = "bp")  # Breusch-Pagan LM test
+print(lm_test)
